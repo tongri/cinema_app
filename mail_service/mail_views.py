515cade0f5599b17e11bb698c462717c996e45b7
sickboi <nikita.tsepochko20@gmail.com>
2023-06-28 10:37:40 +0300

    updated connection and sending message

--- mail_service/mail_views.py
+++ mail_service/mail_views.py
@@ -6,13 +6,24 @@ import aiosmtplib
 import json
 
 
+async def get_connection():
+    try:
+        smtp_client = aiosmtplib.SMTP(hostname="smtp.gmail.com", port=465, use_tls=True)
+        await smtp_client.connect()
+        await smtp_client.login('test@gmail.com', 'password')
+    except Exception as e:
+        print(f"Exception raised {e}, check  credentials or email service configuration")
+
+
 async def send_message(body, mail_to):
     message = EmailMessage()
     message["From"] = "uacinema.nure@localhost"
-    message["To"] = "uacinema.nure@gmail.com"
+    message["To"] = mail_to
     message["Subject"] = "Activity on UA Cinema"
+    message.set_content(body)
 
-    await aiosmtplib.send(message, hostname="127.0.0.1", port=25)
+    async with get_connection() as client:
+        await client.send(message, hostname="smtp.gmail.com", port=465, use_tls=True)
 
 
 def prepare_message(message: str):
@@ -26,11 +37,11 @@ async def main():
     consumer = aioredis.from_url("redis://localhost", decode_responses=True).pubsub()
     await consumer.subscribe('auth_topic')
 
+    await get_connection()
+
     async for message in consumer.listen():
         if isinstance(message['data'], str):
             mail_to, message = prepare_message(message['data'])
-            print(message)
-            print(mail_to)
             if not mail_to or not message:
                 continue
             await send_message(message, mail_to)
