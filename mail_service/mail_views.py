eba816f611d73c9ebf283b04c0339f59ea17cf8a
sickboi <nikita.tsepochko20@gmail.com>
2023-06-28 10:57:18 +0300

    send_message connection and sending updated

--- mail_service/mail_views.py
+++ mail_service/mail_views.py
@@ -6,24 +6,20 @@ import aiosmtplib
 import json
 
 
-async def get_connection():
-    try:
-        smtp_client = aiosmtplib.SMTP(hostname="smtp.gmail.com", port=465, use_tls=True)
-        await smtp_client.connect()
-        await smtp_client.login('test@gmail.com', 'password')
-    except Exception as e:
-        print(f"Exception raised {e}, check  credentials or email service configuration")
-
-
 async def send_message(body, mail_to):
     message = EmailMessage()
     message["From"] = "uacinema.nure@localhost"
-    message["To"] = mail_to
+    message["To"] = "uacinema.nure@gmail.com"
     message["Subject"] = "Activity on UA Cinema"
     message.set_content(body)
 
-    async with get_connection() as client:
-        await client.send(message, hostname="smtp.gmail.com", port=465, use_tls=True)
+    try:
+        async with aiosmtplib.SMTP(hostname="smtp.gmail.com", port=465, use_tls=True) as smtp:
+            await smtp.connect()
+            await smtp.login('test@gmail.com', 'password', timeout=60)  # gmail credentials
+            await smtp.send_message(message)
+    except (aiosmtplib.SMTPAuthenticationError, aiosmtplib.SMTPException) as e:
+        print(f"Exception raised {e}, check  credentials or email service configuration")
 
 
 def prepare_message(message: str):
@@ -36,8 +32,6 @@ def prepare_message(message: str):
 async def main():
     consumer = aioredis.from_url("redis://localhost", decode_responses=True).pubsub()
     await consumer.subscribe('auth_topic')
-
-    await get_connection()
 
     async for message in consumer.listen():
         if isinstance(message['data'], str):
